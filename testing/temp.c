2fdca4cee6bc679172e1b604cc086a05460d997b
Jackson Godsey <era@Jacksons-MacBook-Air.local>
2022-09-13 09:47:28 -0800

    added some code from 9/13 class

--- testing/temp.c
+++ testing/temp.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+int main(void) {
+
+    //rand()% 100 + 20 gets you random numbers between 20 - 29
+
+    int i;
+    int Fibonacci[15];
+
+    Fibonacci[0] = 0;
+    Fibonacci[1] = 1;
+
+    for (i = 2; i < 15; i++) {
+        Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i-2];
+        printf("%d\n", Fibonacci[i]);
+    }
+
+
+}
