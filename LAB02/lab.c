dd5a1c11bc2f0e286627aff0e11b9c9dcf272b9e
Jackson Godsey <era@pop-os.localdomain>
2022-09-15 22:37:46 -0800

    updating

---
+++ LAB02/lab.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int t, o, g, u;
+
+    for (t = 0; t < 10; t++) {
+        for (o = 0; o < 10; o++) {
+            for (g = 0; g < 10; g++) {
+                for (u = 0; u < 10; u++) {
+
+                }
+            }
+        }
+    }
+
+}
